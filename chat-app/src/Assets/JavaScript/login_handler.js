beebe1effb6d8909b7b8475a322f2ac9ba6375d2
David <141311953+Wiggleyyyy@users.noreply.github.com>
2024-07-30 13:19:14 +0200

    Delete chat-app directory

--- chat-app/src/Assets/JavaScript/login_handler.js
+++
@@ -1,6 +0,0 @@
-// Checks if username is stored in session data, else go to login page
-window.onload = function () {
-    if (!sessionStorage.getItem("Username")){
-        window.location.href = "./Pages/login.html"
-    }
-};
